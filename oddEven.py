820c63313e3a7a53cc1069fea80de5566171a2d1
skjalal123 <31567392+skjalal123@users.noreply.github.com>
2020-07-01 19:31:38 +0530

    Update oddEven.py

--- oddEven.py
+++ oddEven.py
@@ -1,9 +1,11 @@
 def oddEven(n,what):
-  num = n / 2
+  num = int(n / 2)
   if what == "odd":
-    for i in range(n):
+    for i in range(num):
       print( 2 * i + 1 )
       
   if what == "even":
-    for i in range(1, n+1):
+    for i in range(1, num+1):
       print( 2 * i)
+
+oddEven(50,"even")
